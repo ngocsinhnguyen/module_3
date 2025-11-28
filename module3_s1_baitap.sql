b34192a44fd9173de4126160096123d07e8d4ccc
ngocsinhnguyen <nguyenns6802@gmail.com>
2025-11-28 15:06:01 +0700

    Initial commit - Add module 3 assignment

---
+++ module3_s1_baitap.sql
@@ -0,0 +1,13 @@
+use bai_tap_1;
+
+create table students(
+	id int,
+    name varchar(100)
+);
+
+create table teachers(
+	id int,
+    name varchar(100),
+    age int,
+    country varchar(100)
+);
