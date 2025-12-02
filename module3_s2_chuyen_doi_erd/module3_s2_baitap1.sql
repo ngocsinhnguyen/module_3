8669986376be8e287ba0f444782f5f57423be0c9
ngocsinhnguyen <nguyenns6802@gmail.com>
2025-12-02 11:10:17 +0700

    s2

--- module3_s2_chuyen_doi_erd/module3_s2_baitap1.sql
+++ module3_s2_chuyen_doi_erd/module3_s2_baitap1.sql
@@ -1,70 +1,70 @@
-create database chuyen_doi_ERD;
+create database chuyen_doi_erd;
 
-use chuyen_doi_ERD;
+use chuyen_doi_erd;
 
-create table PHIEUXUAT (
-    SoPX char(10),
-    NgayXuat date,
-    primary key(SoPX)
+create table phieu_xuat (
+    so_px char(10),
+    ngay_xuat date,
+    primary key(so_px)
 );
 
-create table PHIEUNHAP (
-    SoPN char(10),
-    NgayNhap date,
-    primary key(SoPN)
+create table phieu_nhap (
+    so_pn char(10),
+    ngay_nhap date,
+    primary key(so_pn)
 );
 
-create table VATTU (
-    MaVTU char(10),
-    TenVTU varchar(50),
-    primary key(MaVTU)
+create table vat_tu (
+    ma_vtu char(10),
+    ten_vtu varchar(50),
+    primary key(ma_vtu)
 );
 
-create table NHACC (
-    MaNCC char(10),
-    TenNCC varchar(50),
-    DiaChi varchar(100),
-    SDT varchar(15),
-    primary key(MaNCC)
+create table nha_cc (
+    ma_ncc char(10),
+    ten_ncc varchar(50),
+    dia_chi varchar(100),
+    sdt varchar(15),
+    primary key(ma_ncc)
 );
  
-create table DONDH (
-    SoDH char(10),
-    NgayDH date,
-    MaNCC char(10),
-    primary key(SoDH),
-    foreign key (MaNCC) references NHACC(MaNCC)
+create table don_dh (
+    so_dh char(10),
+    ngay_dh date,
+    ma_ncc char(10),
+    primary key(so_dh),
+    foreign key (ma_ncc) references nha_cc(ma_ncc)
 ); 
 
 -- PHIEUXUAT và VATTU
-create table CHITIET_PHIEUXUAT (
-    SoPX char(10),
-    MaVTU char(10),
-    DGXuat decimal(18, 2), 
-    SLXuat int,
-    primary key (SoPX, MaVTU),
-    foreign key (SoPX) references PHIEUXUAT(SoPX),
-    foreign key (MaVTU) references VATTU(MaVTU)
+create table chi_tiet_phieu_xuat (
+    so_px char(10),
+    ma_vtu char(10),
+    dg_xuat decimal(18, 2), 
+    sl_xuat int,
+    primary key (so_px, ma_vtu),
+    foreign key (so_px) references phieu_xuat(so_px),
+    foreign key (ma_vtu) references vat_tu(ma_vtu)
 );
 
 --  PHIEUNHAP và VATTU
-create table CHITIET_PHIEUNHAP (
-    SoPN char(10),
-    MaVTU char(10),
-    DGNhap decimal(18, 2),
-    SLNhap int,            
-    primary key (SoPN, MaVTU),
-    foreign key (SoPN) references PHIEUNHAP(SoPN),
-    foreign key (MaVTU) references VATTU(MaVTU)
+create table chi_tiet_phieu_nhap (
+    so_pn char(10),
+    ma_vtu char(10),
+    dg_nhap decimal(18, 2),
+    sl_nhap int,            
+    primary key (so_pn, ma_vtu),
+    foreign key (so_pn) references phieu_nhap(so_pn),
+    foreign key (ma_vtu) references vat_tu(ma_vtu)
 );
 
 --  VATTU và DONDH
-create table CHITIET_DONDH (
-    MaVTU char(10),
-    SoDH char(10),
-    primary key (MaVTU, SoDH),
-    foreign key (MaVTU) references VATTU(MaVTU),
-    foreign key (SoDH) references DONDH(SoDH)
+create table chi_tiet_don_dh (
+    ma_vtu char(10),
+    so_dh char(10),
+    primary key (ma_vtu, so_dh),
+    foreign key (ma_vtu) references vat_tu(ma_vtu),
+    foreign key (so_dh) references don_dh(so_dh)
 );
 
 
--- module3_s2_quan_ly_ban_hang.sql
+++ module3_s2_quan_ly_ban_hang.sql
@@ -3,34 +3,34 @@ create database s2_quan_ly_ban_hang;
 use s2_quan_ly_ban_hang;
 
 
-create table Customer (
-    cID int,
-    cName varchar(100) not null,
-    cAge int check (cAge > 0),
-    primary key(cID)
+create table customer (
+    c_id int,
+    c_name varchar(100) not null,
+    c_age int check (c_age > 0),
+    primary key(c_id)
 );
 
-create table Product (
-    pID int,
-    pName varchar(100) not null,
-    pPrice decimal(10, 2) not null check (pPrice >= 0),
-    primary key(pID)
+create table product (
+    p_id int,
+    p_name varchar(100) not null,
+    p_price decimal(10, 2) not null check (p_price >= 0),
+    primary key(p_id)
 );
 
-create table `Order` ( 
-    oID int , 
-    cID int not null, 
-    oDate date not null, 
-    oTotalPrice decimal(10, 2),
-    foreign key (cID) references Customer(cID),
-    primary key(oID)
+create table `order` ( 
+    o_id int , 
+    c_id int not null, 
+    o_date date not null, 
+    o_total_price decimal(10, 2),
+    foreign key (c_id) references customer(c_id),
+    primary key(o_id)
 );
 
-create table OrderDetail (
-    oID int,
-    pID int, 
-    odQTY int not null check (odQTY > 0) , 
-    primary key (oID, pID), 
-    foreign key (oID) references `Order`(oID), 
-    foreign key (pID) references Product(pID)
+create table order_detail (
+    o_id int,
+    p_id int, 
+    od_q_t_y int not null check (od_q_t_y > 0) , 
+    primary key (o_id, p_id), 
+    foreign key (o_id) references `order`(o_id), 
+    foreign key (p_id) references product(p_id)
 );
